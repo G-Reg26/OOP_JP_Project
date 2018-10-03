c7f82192e4979e48de700ad77234fb0022d574c4
Gregorio Lozada <greglozada@gmail.com>
2018-10-02 22:49:09 -0400

    OOP Program Alpha Commit 10/02/2018 Put ItemType enum in a separate file.

--- src/Item.java
+++ src/Item.java
@@ -1,35 +1,13 @@
 /**
  * Gregorio Lozada Sept 29, 2018
  *
- * ItemType: Enum that contains values for each type of item as well as a type's code. It also has a
- * constructor that takes in a string called code and sets the constant field of the same name to
- * it.
- *
  * Item: Interface that contains a constant string called manufacturer, as well as getters and
  * setters for serial number, name, and manufacture date.
  *
- * Instructions to create both the enum and interface were provided by Oracle Academy.
+ * Instructions to create the interface was provided by Oracle Academy.
  */
 
 import java.util.Date;
-
-enum ItemType {
-  Audio("AU"),
-  Visual("VI"),
-  AudioMobile("AM"),
-  VisualMobile("VM");
-
-  public final String code;
-
-  /**
-   * Sets the constant field code to the string parameter of the same name
-   *
-   * @param code the code for the item type
-   */
-  ItemType(String code) {
-    this.code = code;
-  }
-}
 
 public interface Item {
 
